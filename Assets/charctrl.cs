930ddbb18507d15046b4d2859aa2ccd6f0bd67fe
Blieper <yoranmandema@eml.cc>
2018-04-24 22:05:57 +0200

    Camera turning

--- Assets/charctrl.cs
+++ Assets/charctrl.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class charctrl : MonoBehaviour {
+    public GameObject cameraSlave;
+    public Camera cam;
     public CharacterController characterctrl;
     public bool isjumping = false;
     Vector3 movedirection = Vector3.zero;
+    Vector3 cameraRotateAngle = Vector3.forward;
     public float movementspeed = 1f;
     public float jumpSpeed = 1f;
 
     // Use this for initialization
     void Start () {
         characterctrl = GetComponent<CharacterController>();
-        
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 	
 	// Update is called once per frame
@@ -35,5 +39,15 @@ public class charctrl : MonoBehaviour {
             movedirection.y += Physics.gravity.y * Time.deltaTime;
         }
         characterctrl.Move(movedirection * Time.deltaTime);
+
+        transform.RotateAround(transform.position, transform.up, Input.GetAxis("Mouse X"));
+
+        cameraRotateAngle = cameraSlave.transform.right * -Input.GetAxis("Mouse Y");
+        cameraSlave.transform.RotateAround(transform.position, cameraRotateAngle, cameraRotateAngle.magnitude);
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
